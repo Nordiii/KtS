bbd2cc0714c22dab3931fbc2939113b15eb786c3
Nordiii <niklas907@googlemail.com>
2016-04-11 15:41:33 +0200

    weapon script

---
+++ Assets/Scripts/Ammunition.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ammunition : MonoBehaviour {
+
+    public float ammunition_speed;
+    public float ammunition_damage;
+
+	// Use this for initialization
+	void Start () {
+        Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
+
+        if (rb == null)
+            Debug.Log("Kein Rigidbody vorhanden");
+
+        rb.AddForce(new Vector2(0, 700));
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+       
+        
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("LOL");
+    }
+
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour {
+
+    public string weapon_Name;
+
+    public float reload_Time_Second;
+    public float shoot_interval_Second;
+
+    public bool infinite_Ammunition;
+
+    public int weapon_Reload_Ammunition;
+    public int weapon_Magazin_Ammunition;
+    public int weapon_Magazin_size;
+
+    public GameObject bullet;
+
+    private float current_threshold;
+    private float shoot_timer;
+    private bool able_to_shoot;
+
+    private Vector2 start;
+    private Vector2 target;
+
+	void Start ()
+    {
+        current_threshold = 0;
+        shoot_timer = 0;
+       
+        able_to_shoot = true;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(!able_to_shoot)
+        {
+            if (!(shoot_timer >= current_threshold))
+            {
+                shoot_timer += Time.deltaTime;
+                //Debug.Log(shoot_timer);
+            }
+            else
+            {
+                able_to_shoot = true;
+                shoot_timer = 0;
+            }
+               
+        }
+        Debug.Log(Input.GetMouseButton(0));
+        if (able_to_shoot && Input.GetMouseButton(0))
+            shot();
+
+        
+	}
+
+    void shot()
+    {
+       // Debug.Log("TEST");
+        able_to_shoot = false;
+        current_threshold = shoot_interval_Second;
+
+        GameObject projectile = (GameObject)Instantiate(bullet, transform.position, transform.rotation);
+       // Debug.Log(projectile.transform.position);
+        return;
+    }
+
+
+}
