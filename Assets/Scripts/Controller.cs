b8414433824569a3491971836f91c02d2f7c6bd1
Jonas <jonas@meiers.net>
2016-04-11 12:50:03 +0200

    New Upload Player And Follow

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+	public float moveSpeed = 3.0f; //speed player moves
+	public float gravtiy = 9.81f;
+
+	private CharacterController myController;
+
+	Vector3 movementZ;
+	Vector3 movementx;
+	Vector3 movement;
+
+	// Use this for initialization
+	void Start () {
+		myController = gameObject.GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move ();
+	}
+
+
+
+	void Move()
+	{
+		movementZ = Input.GetAxis ("Vertical") * Vector3.forward * moveSpeed * Time.deltaTime;
+		movementx = Input.GetAxis ("Horizontal") * Vector3.right * moveSpeed * Time.deltaTime;
+		movement = transform.TransformDirection (movementZ + movementx);
+		movement.y -= gravtiy * Time.deltaTime;
+
+		myController.Move (movement);
+	}
+}
