9c8e498b58b59842e8ead9ba01d83104428982e0
Nordiii <niklas907@googlemail.com>
2016-04-13 23:53:34 +0200

    Waffe feuert nun richtung Mauszeiger|Waffe muss jetzt nachgeladen werden

--- Assets/Scripts/Ammunition.cs
+++ Assets/Scripts/Ammunition.cs
@@ -3,31 +3,77 @@ using System.Collections;
 
 public class Ammunition : MonoBehaviour {
 
-    public float ammunition_speed;
-    public float ammunition_damage;
+    public float ammunition_Speed = 100;
+    public float ammunition_Damage = 10;
+    public float time_Till_Destroy = 5;
+
+    private Vector2 mousePos;
+    private Vector2 bulletPos;
+    private Vector2 movement;
 
 	// Use this for initialization
-	void Start () {
-        Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
+	void Start ()
+    {
+     
+        //Speichert die Mausposition mit den koordinaten auf der Spielkarte
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
+        //speichert die koordinate wo die kugel gespawnt ist
+        bulletPos = gameObject.transform.position;
+
+        //Sucht nach dem rigidbody des Objektes um später die Kraft hinzuzufügen
+        Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
+        
+        
         if (rb == null)
             Debug.Log("Kein Rigidbody vorhanden");
+        //Normalisiert den richtungs Vektor damit egal wie die Entfernung zur Maus ist die Kugel die gleiche geschwindigkeit hat.
+        Vector2 norma = normaVector(new Vector2(calculateX(), calculateY()));
 
-        rb.AddForce(new Vector2(0, 700));
-
+        //Rechnet die gewünschte Geschwindigkeit ein
+        norma = new Vector2(norma.x * ammunition_Speed, norma.y * ammunition_Speed);
+        
+        
+        rb.AddForce(norma);
+        
     }
-
+    
+    private float timeToDestroyCounter = 0;
     // Update is called once per frame
+
+
     void Update ()
     {
-       
-        
+        timeToDestroyCounter += Time.deltaTime;
 
+        if(timeToDestroyCounter >= time_Till_Destroy)
+        {
+            timeToDestroyCounter = 0;
+            Destroy(gameObject);
+        }
+    }
+
+    private Vector2 normaVector(Vector2 richtung)
+    {
+        float I = Mathf.Sqrt(Mathf.Pow(richtung.x,2)+Mathf.Pow(richtung.y,2));
+        return new Vector2(richtung.x / I, richtung.y / I);
+    }
+
+    private float calculateX()
+    {
+        return mousePos.x - bulletPos.x;
+    }
+
+    private float calculateY()
+    {
+        return mousePos.y - bulletPos.y;
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("LOL");
+        //Fügt die geschwindigkeit hinzu bevor das gameObject zerstört wird (knockback)
+        collision.rigidbody.AddForce(collision.relativeVelocity);
+        Destroy(gameObject);
     }
 
 }
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -6,19 +6,20 @@ public class Weapon : MonoBehaviour {
     public string weapon_Name;
 
     public float reload_Time_Second;
-    public float shoot_interval_Second;
+    public float shoot_Interval_Second;
 
     public bool infinite_Ammunition;
 
-    public int weapon_Reload_Ammunition;
-    public int weapon_Magazin_Ammunition;
-    public int weapon_Magazin_size;
+    public int weapon_Reload_Ammunition = 1;
+    public int weapon_Magazin_Ammunition = 0;
+    public int weapon_Magazin_Size = 1;
 
     public GameObject bullet;
 
     private float current_threshold;
     private float shoot_timer;
     private bool able_to_shoot;
+    private bool reload;
 
     private Vector2 start;
     private Vector2 target;
@@ -27,43 +28,69 @@ public class Weapon : MonoBehaviour {
     {
         current_threshold = 0;
         shoot_timer = 0;
-       
+        reload = false;
         able_to_shoot = true;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
+      
         if(!able_to_shoot)
         {
             if (!(shoot_timer >= current_threshold))
             {
                 shoot_timer += Time.deltaTime;
-                //Debug.Log(shoot_timer);
             }
             else
             {
                 able_to_shoot = true;
                 shoot_timer = 0;
+                if (reload && infinite_Ammunition)
+                    weapon_Magazin_Ammunition = weapon_Magazin_Size;
+                else if (reload)
+                   if(weapon_Magazin_Size<weapon_Reload_Ammunition)
+                    {
+                        weapon_Magazin_Ammunition = weapon_Magazin_Size;
+                        weapon_Reload_Ammunition -= weapon_Magazin_Size;
+                    }
+                    else
+                    {
+                        weapon_Magazin_Ammunition = weapon_Reload_Ammunition;
+                        weapon_Reload_Ammunition = 0;
+                    }
+                reload = false;
             }
                
         }
-        Debug.Log(Input.GetMouseButton(0));
+       //Debug.Log(Input.GetMouseButton(0));
         if (able_to_shoot && Input.GetMouseButton(0))
             shot();
 
         
 	}
 
-    void shot()
+   private void shot()
     {
-       // Debug.Log("TEST");
         able_to_shoot = false;
-        current_threshold = shoot_interval_Second;
 
-        GameObject projectile = (GameObject)Instantiate(bullet, transform.position, transform.rotation);
-       // Debug.Log(projectile.transform.position);
+        if (weapon_Magazin_Ammunition != 0)
+        {
+            current_threshold = shoot_Interval_Second;
+            GameObject projectile = (GameObject)Instantiate(bullet, transform.position, transform.rotation);
+
+            weapon_Magazin_Ammunition--;
+            
+        }
+
+
+        if(weapon_Magazin_Ammunition == 0)
+        {
+            reload = true;
+            current_threshold = reload_Time_Second;
+        }
         return;
+
     }
 
 
