521ebcf4e2de41496b951ddbafd1ad0c63bebf69
Jonas <jonas@meiers.net>
2016-04-20 16:47:27 +0200

    Spawner

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager gm;
+	public float score = 0f;
+
+	void Awake(){
+		if (gm == null) {
+			gm = this.GetComponent<GameManager> ();
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+	public GameObject[] enemy;
+
+	public int counter = 20;
+	public float spawnMin = 1f; //wann spawnt nächster Gegner
+	public float spawnMax = 3f;
+
+	float time;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(Spawn ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator Spawn(){
+		while (counter-- != 0) {
+			Instantiate (enemy [Random.Range (0, enemy.Length)], transform.position, Quaternion.identity);
+			yield return new WaitForSeconds(1);
+		}
+	}
+}
