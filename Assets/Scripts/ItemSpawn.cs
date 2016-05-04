316ffdd40859c8c6ad529efa0ce8189e8e28c4d8
Niklas Nordmann <niklas.nordmann@smail.inf.h-brs.de>
2016-05-04 12:02:38 +0200

    item spawner spawnt jetzt waffen

--- Assets/Scripts/ItemSpawn.cs
+++ Assets/Scripts/ItemSpawn.cs
@@ -7,6 +7,8 @@ public class ItemSpawn : MonoBehaviour
     public GameObject[] weapon_Prefab;
     public float spawn_time;
 
+    private GameObject current_item;
+
     private bool empty = true;
     private float timer = 0;
 
@@ -23,13 +25,32 @@ public class ItemSpawn : MonoBehaviour
         if (timer >= spawn_time && empty)
         {
             Random rnd = new Random();
-            GameObject item = (GameObject)Instantiate(weapon_Prefab[Random.Range(0,weapon_Prefab.Length)], transform.position - new Vector3(0, 0.5F, 0), transform.rotation);
-            item.GetComponent<Weapon>().enabled = false;
-            item.GetComponent<WeaponRotation>().enabled = false;
+            current_item = (GameObject)Instantiate(weapon_Prefab[Random.Range(0,weapon_Prefab.Length)], transform.position - new Vector3(0, 0.5F, 0), transform.rotation);
+            current_item.GetComponent<Weapon>().enabled = false;
+            current_item.GetComponent<WeaponRotation>().enabled = false;
 
-            item.GetComponent<Transform>().localScale = new Vector3(2, 2, 1);
+            current_item.GetComponent<Transform>().localScale = new Vector3(2, 2, 1);
             empty = false;
         }
         timer += Time.deltaTime;
 	}
+
+    
+    public void OnTriggerEnter2D(Collider2D coll)
+    {
+        if(coll.tag.Equals("Player"))
+        {
+            Vector3 position = coll.transform.GetChild(1).gameObject.transform.position;
+            Destroy(coll.transform.GetChild(1).gameObject);
+            current_item.transform.parent = coll.transform;
+            current_item.transform.position = position;
+            current_item.GetComponent<Weapon>().enabled = true;
+            current_item.GetComponent<WeaponRotation>().enabled = true;
+
+
+            empty = true;
+            timer = 0;
+        }
+       
+    }
 }
