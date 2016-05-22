1d5a4896e709f8a513747262108b34b0613a9615
Niklas Nordmann <niklas.nordmann@smail.inf.h-brs.de>
2016-05-22 18:24:24 +0200

    SwitchWeaponFix | deagle prefab -> deagel sound

--- Assets/Scripts/Weapon/SwitchToDefaultWeapon.cs
+++ Assets/Scripts/Weapon/SwitchToDefaultWeapon.cs
@@ -18,10 +18,10 @@ public class SwitchToDefaultWeapon : MonoBehaviour {
     public void switchOut()
     {
 
-            Vector3 position = GameObject.Find("Player").transform.GetChild(1).gameObject.transform.position;
-             Destroy(GameObject.Find("Player").transform.GetChild(1).gameObject);
-            GameObject current_item = (GameObject)Instantiate(default_weapon, transform.position - new Vector3(0, 0.5F, 0), transform.rotation);
-            current_item.transform.parent = GameObject.Find("Player").transform;
+        Vector3 position = GameObject.Find("Player").GetComponentInChildren<Weapon>().gameObject.transform.position;
+        Destroy(GameObject.Find("Player").GetComponentInChildren<Weapon>().gameObject);
+        GameObject current_item = (GameObject)Instantiate(default_weapon, transform.position - new Vector3(0, 0.5F, 0), transform.rotation);
+        current_item.transform.parent = GameObject.Find("Player").transform;
             current_item.transform.localScale = default_weapon.transform.lossyScale;
             current_item.transform.position = position;
     }
